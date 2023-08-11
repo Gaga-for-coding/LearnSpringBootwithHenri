a23d4fa5b376c20cfcd0b0fbf98d358b3c559efd
Henri-joel Guissi <j.guissi@fintechandpaymentsinstitute.com>
2023-08-11 23:01:21 +0000

    Initial commit 👽

--- src/test/java/com/guissisoftware/learnspringboot/LearnSpringBootApplicationTests.java
+++ src/test/java/com/guissisoftware/learnspringboot/LearnSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.guissisoftware.learnSpringBoot;
+package com.guissisoftware.learnspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
